1eeea03c8bd6aebf4d69db4803d0f233b1d93d72
zhuoyikang <zhuoyikang@gmail.com>
2017-02-10 10:51:05 +0800

    new

--- src/conn.erl
+++ src/conn.erl
@@ -17,90 +17,91 @@
           socket,                        %% 与进程关联的 socket
           player_id,                     %% 玩家ID
           keep_still_times = 0           %% 超越次数
-  	}).
+         }).
 
 -include("proto.hrl").
 
 %% 启动gen_server
 start_link(Socket) ->
-  gen_server:start_link(?MODULE, [Socket], []).
+    gen_server:start_link(?MODULE, [Socket], []).
 
 %% 停止gen_server
 stop(Ref) ->
-  gen_server:cast(Ref, stop).
+    gen_server:cast(Ref, stop).
 
-%% 检查连接的数据包
-heart_beat(Pid) -> 
-  gen_server:call(Pid, heart_beat).
+%% %% 检查连接的数据包
+%% heart_beat(Pid) ->
+%%     gen_server:call(Pid, heart_beat).
 
 init([Socket]) ->
-  process_flag(trap_exit, true),
-  inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
-  State = #state{socket = Socket},
-  erlang:put(socket, Socket),
-  {ok, State}.
+    process_flag(trap_exit, true),
+    inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
+    State = #state{socket = Socket},
+    erlang:put(socket, Socket),
+    {ok, State}.
 
 handle_cast(Cast, State) ->
-  State1 = conn_config:exec(cast, [Cast, State]),
-  {noreply, State1}.
+    State1 = conn_config:exec(cast, [Cast, State]),
+    {noreply, State1}.
 
 %% 检查连接是否超时仍为有数据通信
-handle_call(heart_beat, _From, State = #state{socket = Socket,
-  recv_cnt = ReceiveBags, keep_still_times = LostTimes}) ->
-  NowConnBags = inet:getstat(Socket, [recv_cnt]),
-  case ReceiveBags == NowConnBags of
-    true -> NewSate = #state{socket = Socket, keep_still_times = LostTimes + 1};
-    false -> NewState = #state{socket = Socket, recv_cnt = NowConnBags, keep_still_times = 0}
-  end,
-  case LostTimes >= ?LOSTTIME of
-    true -> gen_tcp:close(Socket),
-      {stop, timeout, NewState};
-    false -> {noreply, NewState}
-  end.
+handle_call(heart_beat, _From, _State = #state{socket = Socket,
+                                               recv_cnt = ReceiveBags, keep_still_times = LostTimes}) ->
+    NowConnBags = inet:getstat(Socket, [recv_cnt]),
+    NewState =
+        case ReceiveBags == NowConnBags of
+            true ->  #state{socket = Socket, keep_still_times = LostTimes + 1};
+            false -> #state{socket = Socket, recv_cnt = NowConnBags, keep_still_times = 0}
+        end,
+    case LostTimes >= 10 of
+        true -> gen_tcp:close(Socket),
+                {stop, timeout, NewState};
+        false -> {noreply, NewState}
+    end;
 
 handle_call(Call, From, State) ->
-  State1 = conn_config:exec(cast, [Call, From, State]),
-  {reply, ok, State1}.
+    State1 = conn_config:exec(cast, [Call, From, State]),
+    {reply, ok, State1}.
 
 handle_info({tcp_closed, Socket}, State)
   when Socket == State#state.socket ->
-  {stop, normal, State};
+    {stop, normal, State};
 
 handle_info({tcp_error, _Port, _Reason}, State) ->
-  {noreply, State};
+    {noreply, State};
 
 
 handle_info({tcp, Socket, Bin}, State) ->
-  inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
-  <<Type:?HWORD, RequestData/binary>> = Bin,
-  ?M(Payload) = proto_decoder:decode(Type, RequestData),
-  {M, F} = proto_route:route(Type),
-  M:F(Payload),
-  keep_alive_or_close(keep_alive, State);
+    inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
+    <<Type:?HWORD, RequestData/binary>> = Bin,
+    ?M(Payload) = proto_decoder:decode(Type, RequestData),
+    {M, F} = proto_route:route(Type),
+    M:F(Payload),
+    keep_alive_or_close(keep_alive, State);
 
 handle_info(Info, State) ->
-  conn_config:exec(info, [Info, State]),
-  {noreply, State}.
+    conn_config:exec(info, [Info, State]),
+    {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 terminate(Reason, State) ->
-  conn_config:exec(quit, [Reason, State]),
-  ok.
+    conn_config:exec(quit, [Reason, State]),
+    ok.
 
 keep_alive_or_close(Keep, State) ->
-  if
-    Keep /= keep_alive -> gen_tcp:close(State#state.socket),
-                          {stop, normal, State};
-    true -> {noreply, State}
-  end.
+    if
+        Keep /= keep_alive -> gen_tcp:close(State#state.socket),
+                              {stop, normal, State};
+        true -> {noreply, State}
+    end.
 
 %% 一些副作用代码
 send(TypeAtom, Paylod) ->
-  Socket = erlang:get(socket),
-  Bin = proto_encoder:encode(TypeAtom, Paylod),
-  gen_tcp:send(Socket, Bin),
-  ok.
+    Socket = erlang:get(socket),
+    Bin = proto_encoder:encode(TypeAtom, Paylod),
+    gen_tcp:send(Socket, Bin),
+    ok.
 
-  
-  
+
+
